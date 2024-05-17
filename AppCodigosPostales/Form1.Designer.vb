d009f75e0439e44c17b5e1fc0de1698fd57188ee
AGUSTIN BUSTOS SALAZAR <agustinbustos@uadec.edu.mx>
2024-05-16 19:13:05 -0600

    Agregar archivos de proyecto.

---
+++ AppCodigosPostales/Form1.Designer.vb
@@ -0,0 +1,94 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+  Inherits System.Windows.Forms.Form
+
+  'Form overrides dispose to clean up the component list.
+  <System.Diagnostics.DebuggerNonUserCode()>
+  Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+    Try
+      If disposing AndAlso components IsNot Nothing Then
+        components.Dispose()
+      End If
+    Finally
+      MyBase.Dispose(disposing)
+    End Try
+  End Sub
+
+  'Required by the Windows Form Designer
+  Private components As System.ComponentModel.IContainer
+
+  'NOTE: The following procedure is required by the Windows Form Designer
+  'It can be modified using the Windows Form Designer.  
+  'Do not modify it using the code editor.
+  <System.Diagnostics.DebuggerStepThrough()>
+  Private Sub InitializeComponent()
+    Dim DataGridViewCellStyle1 As DataGridViewCellStyle = New DataGridViewCellStyle()
+    Dim DataGridViewCellStyle2 As DataGridViewCellStyle = New DataGridViewCellStyle()
+    DataGridView1 = New DataGridView()
+    cboEstado = New ComboBox()
+    cboMunicipio = New ComboBox()
+    CType(DataGridView1, ComponentModel.ISupportInitialize).BeginInit()
+    SuspendLayout()
+    ' 
+    ' DataGridView1
+    ' 
+    DataGridView1.Anchor = AnchorStyles.Top Or AnchorStyles.Bottom Or AnchorStyles.Left Or AnchorStyles.Right
+    DataGridViewCellStyle1.Alignment = DataGridViewContentAlignment.MiddleLeft
+    DataGridViewCellStyle1.BackColor = SystemColors.Control
+    DataGridViewCellStyle1.Font = New Font("Segoe UI", 20.25F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+    DataGridViewCellStyle1.ForeColor = SystemColors.WindowText
+    DataGridViewCellStyle1.SelectionBackColor = SystemColors.Highlight
+    DataGridViewCellStyle1.SelectionForeColor = SystemColors.HighlightText
+    DataGridViewCellStyle1.WrapMode = DataGridViewTriState.True
+    DataGridView1.ColumnHeadersDefaultCellStyle = DataGridViewCellStyle1
+    DataGridView1.ColumnHeadersHeight = 50
+    DataGridViewCellStyle2.Alignment = DataGridViewContentAlignment.MiddleLeft
+    DataGridViewCellStyle2.BackColor = SystemColors.Window
+    DataGridViewCellStyle2.Font = New Font("Segoe UI", 20F)
+    DataGridViewCellStyle2.ForeColor = SystemColors.ControlText
+    DataGridViewCellStyle2.SelectionBackColor = SystemColors.Highlight
+    DataGridViewCellStyle2.SelectionForeColor = SystemColors.HighlightText
+    DataGridViewCellStyle2.WrapMode = DataGridViewTriState.False
+    DataGridView1.DefaultCellStyle = DataGridViewCellStyle2
+    DataGridView1.Location = New Point(12, 100)
+    DataGridView1.Name = "DataGridView1"
+    DataGridView1.Size = New Size(776, 338)
+    DataGridView1.TabIndex = 0
+    ' 
+    ' cboEstado
+    ' 
+    cboEstado.Font = New Font("Segoe UI", 16F)
+    cboEstado.FormattingEnabled = True
+    cboEstado.Location = New Point(12, 26)
+    cboEstado.Name = "cboEstado"
+    cboEstado.Size = New Size(255, 38)
+    cboEstado.TabIndex = 1
+    ' 
+    ' cboMunicipio
+    ' 
+    cboMunicipio.Font = New Font("Segoe UI", 16F)
+    cboMunicipio.FormattingEnabled = True
+    cboMunicipio.Location = New Point(288, 26)
+    cboMunicipio.Name = "cboMunicipio"
+    cboMunicipio.Size = New Size(255, 38)
+    cboMunicipio.TabIndex = 2
+    ' 
+    ' Form1
+    ' 
+    AutoScaleDimensions = New SizeF(7F, 15F)
+    AutoScaleMode = AutoScaleMode.Font
+    ClientSize = New Size(800, 450)
+    Controls.Add(cboMunicipio)
+    Controls.Add(cboEstado)
+    Controls.Add(DataGridView1)
+    Name = "Form1"
+    Text = "Form1"
+    CType(DataGridView1, ComponentModel.ISupportInitialize).EndInit()
+    ResumeLayout(False)
+  End Sub
+
+  Friend WithEvents DataGridView1 As DataGridView
+  Friend WithEvents cboEstado As ComboBox
+  Friend WithEvents cboMunicipio As ComboBox
+
+End Class
---
+++ AppCodigosPostales/Form1.vb
@@ -0,0 +1,85 @@
+﻿Imports System.Data.SqlClient
+
+Public Class Form1
+    Dim query As String
+    'Create a constructor
+    Public Sub New()
+        ' This call is required by the designer.
+        InitializeComponent()
+        DataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.DisplayedCells
+        ' Add any initialization after the InitializeComponent() call.
+        'Create a new instance of the Connection class
+
+        query = "select id, nombre from estado"
+        cboEstado.DataSource = Connection.SelectQuery(query)
+        cboEstado.DisplayMember = "nombre"
+        cboEstado.ValueMember = "id"
+
+    End Sub
+
+    Private connectionString As String = "Data Source=LAPTOP-03SGE49J; Initial Catalog=CorreosDeMexico1;Integrated Security=True"
+
+    Private Sub cboEstado_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cboEstado.SelectedIndexChanged
+        If cboEstado.SelectedValue IsNot Nothing Then
+            Dim idEstado As Integer
+            If Integer.TryParse(cboEstado.SelectedValue.ToString(), idEstado) Then
+                CargarMunicipios(idEstado)
+            End If
+        End If
+    End Sub
+
+    Private Sub CargarMunicipios(idEstado As Integer)
+        Dim query As String = "SELECT id, nombre FROM municipio WHERE idEstado = @idEstado"
+
+        Using connection As New SqlConnection(connectionString)
+            Try
+                connection.Open()
+                Using command As New SqlCommand(query, connection)
+                    command.Parameters.Add(New SqlParameter("@idEstado", idEstado))
+
+                    Dim adapter As New SqlDataAdapter(command)
+                    Dim table As New DataTable()
+
+                    adapter.Fill(table)
+
+                    cboMunicipio.DataSource = table
+                    cboMunicipio.DisplayMember = "nombre"
+                    cboMunicipio.ValueMember = "id"
+                End Using
+            Catch ex As Exception
+                MessageBox.Show("Error al cargar municipios: " & ex.Message)
+            End Try
+        End Using
+    End Sub
+
+    Private Sub cboMunicipio_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cboMunicipio.SelectedIndexChanged
+        If String.IsNullOrEmpty(cboMunicipio.Text) Then
+            Exit Sub
+        End If
+
+        Dim nombreMunicipio As String = cboMunicipio.Text
+        CargarDatosMunicipio(nombreMunicipio)
+    End Sub
+
+    Private Sub CargarDatosMunicipio(nombreMunicipio As String)
+        Dim query As String = "SELECT * FROM VnombreBUSQUEDA WHERE [Nombre Municipio] = @nombreMunicipio"
+
+        Using connection As New SqlConnection(connectionString)
+            Try
+                connection.Open()
+                Using command As New SqlCommand(query, connection)
+                    command.Parameters.Add(New SqlParameter("@nombreMunicipio", nombreMunicipio))
+
+                    Dim adapter As New SqlDataAdapter(command)
+                    Dim table As New DataTable()
+
+                    adapter.Fill(table)
+
+                    DataGridView1.DataSource = table
+                End Using
+            Catch ex As Exception
+                MessageBox.Show("Error al cargar datos del municipio: " & ex.Message)
+            End Try
+        End Using
+    End Sub
+End Class
